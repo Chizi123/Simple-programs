d16f756ba5bd4b9ba9135521661ffadb427488b4
Chizi123 <joelgrun@gmail.com>
2015-09-18 19:16:45 +1000

    Create +1.c

---
+++ +1.c
@@ -0,0 +1,15 @@
+/*
+ * Program to add one to a number and print it indefinately
+ * 
+ * By Joel Grunbaum
+ * joelgrun@gmail.com
+**/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Ohh, oh...\n");
+	for (int i = 0; 1; i++)
+		printf("%i\n", i);
+}
